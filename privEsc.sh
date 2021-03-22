466d4c2cdfa5740dbfd34cdbee0e63ab9b233804
Jesse Holland <holland.jesse55@gmail.com>
2021-03-22 15:55:57 -0500

    added other setup scripts

---
+++ privEsc.sh
@@ -0,0 +1,5 @@
+mkdir /home/user/ps/
+cd /home/user/ps/
+sudo echo -e "#include<unistd.h>\\nvoid main()\\n{ setuid(0);\\n setgid(0);\\n system(\"ps\");\\n}"
+sudo gcc demo.c -o shell
+sudo chmod u+s shell
