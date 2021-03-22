aea2979044cf12d74c7fb947c72873aef28256ee
Jesse Holland <holland.jesse55@gmail.com>
2021-03-22 16:32:50 -0500

    updated priv_esc

--- priv_esc.sh
+++ priv_esc.sh
@@ -1,3 +1,5 @@
+sudo apt-get update
+sudo apt install -y gcc
 mkdir /home/user/ps/
 cd /home/user/ps/
 sudo echo -e "#include<unistd.h>\\nvoid main()\\n{ setuid(0);\\n setgid(0);\\n system(\"ps\");\\n}"
