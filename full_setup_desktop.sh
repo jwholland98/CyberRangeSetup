b73b296c97344887e5cdd714fa54633f7ce3390e
Jesse Holland <holland.jesse55@gmail.com>
2021-03-23 10:33:51 -0500

    renaming and moving

---
+++ full_setup_desktop.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt install -y gcc
+mkdir /home/user/ps/
+cd /home/user/ps/
+sudo echo -e "#include<unistd.h>\\nvoid main()\\n{ setuid(0);\\n setgid(0);\\n system(\"ps\");\\n}" > demo.c
+sudo gcc demo.c -o shell
+sudo chmod u+s shell
