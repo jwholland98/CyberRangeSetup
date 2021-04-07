61d3c3cf2bee2bb55e98eaad3f895812ce2333d4
jwholland98 <holland.jesse55@gmail.com>
2021-04-07 10:43:25 -0500

    updated website script

--- website_setup.sh
+++ website_setup.sh
@@ -1,4 +1,7 @@
+cd /home/user/
 git clone --single-branch --branch stable https://gitlab.com/mdlayer42/PompeiiSite.git
 cd PompeiiSite
+sudo chmod +x install_webserver
+sudo chmod +x install_mysql
 sudo ./install_webserver.sh
 sudo ./install_mysql.sh
